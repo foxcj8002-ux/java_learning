cad9c6c4d14f7754b11bbfa18fd4124ac427da25
chenxinlin <foxcj8002@gmail.com>
2026-08-28 15:28:06 +0800

    finish day01 task

---
+++ src/week01/Day01.java
@@ -0,0 +1,28 @@
+package week01;
+import java.util.Scanner;
+
+public class Day01 {
+    public static void main(String[] args) {
+        //p1
+        String name = "foxcj";
+        int age = 18;
+        double height = 170.5;
+        boolean graduateStudent = false;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println("graduateStudent:" + graduateStudent);
+
+        //p2
+        Scanner input = new Scanner(System.in);
+        System.out.println("输入身高(米)：");
+        height = input.nextDouble();
+        System.out.println("输入体重(kg)：");
+        double weight = input.nextDouble();
+
+        System.out.println("你的BMI指数为：" + weight/(height*height));
+
+        input.close();
+    }
+}
